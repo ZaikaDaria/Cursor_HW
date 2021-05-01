2d8e0797ea42867b4022dc363076d76d3dbae82e
Daria_Zaika <dashazaika@gmail.com>
2021-05-01 13:28:49 +0300

    Task2 stating

--- HW2_OOP/Task2.py
+++ HW2_OOP/Task2.py
@@ -1,19 +1,23 @@
-# Create two classes: Person, Cell Phone, one for composition, another one for aggregation.
-#   a.
-      class Person:
-          """
-          Make the class with composition.
-          """
-      class Arm:
-          """
-          Make the class with gi.
-          """
+class Person:
+    def __init__(self, head, body):
+        left_arm = Arm('this is the left one')
+        right_arm = Arm('this is the right one')
+        self.arms = [left_arm, right_arm]
+        self.head = head
+        self.body = body
 
-      class CellPhone:
-          """
-          Make the class with aggregation
-          """
-      class Screen:
-          """
-          Make the class with aggregation
-          """
+class Arm:
+    def __init__(self, hand):
+        self.hand = hand
+
+
+person = Person('round', 'fit')
+for per in person.arms:
+    print(per.hand)
+
+class CellPhone:
+    def __init__(self):
+        self.screen = Screen('6.5"')
+
+class Screen:
+    def __init__(self):
