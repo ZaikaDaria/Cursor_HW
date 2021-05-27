643d017b5abd6e8a2ea71ef883bb81b9e771d7d7
Daria_Zaika <dashazaika@gmail.com>
2021-05-27 20:51:35 +0300

    HW8 finished

---
+++ HW8_Docker/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def main():
+    return "Welcome!"
+
+
+@app.route('/hello')
+def hello():
+    return 'Hi there!'
+
+@app.route('/user')
+def user_name():
+    return 'Daria'
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
