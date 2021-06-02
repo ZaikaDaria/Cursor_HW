fb71d5528b708dcda6f631169aec436a49d60563
Daria_Zaika <dashazaika@gmail.com>
2021-06-02 19:47:37 +0300

    fixing issue with arguments

--- HW5_Tests_Practice/test_winner.py
+++ HW5_Tests_Practice/test_winner.py
@@ -5,23 +5,23 @@ class TestWinner(unittest.TestCase):
 
 
     def setUp(self):
-        self.board = TicTacToe()
+        self.game = TicTacToe()
 
     def test_check_column_win(self):
-        self.board.rows = [['X', ' ', ' '], ['X', ' ', ' '], ['X', ' ', ' ']]
-        self.assertTrue(self.board.winner(6, ' '))
+        self.game.board = [['X', 'X', 'X'], [' ', 'O', ' '], [' ', ' ', ' ']]
+        self.assertTrue(self.game.winner(6, 'X'))
 
     def test_check_row_win(self):
-        self.board.rows = [['X', 'X', 'X'], [' ', ' ', ' '], [' ', ' ', ' ']]
-        self.assertTrue(self.board.winner(6, ' '))
+        self.game.board = ['X', 'X', 'X', ' ', 'O', ' ', ' ', ' ', ' ']
+        self.assertTrue(self.game.winner(0, 'X'))
 
     def test_check_first_diagonal_win(self):
-        self.board.rows = [['X', ' ', ' '], [' ', 'X', ' '], [' ', ' ', 'X']]
-        self.assertTrue(self.board.winner(6, ' '))
+        self.game.board = ['X', ' ', ' ', ' ', 'X', ' ', ' ', ' ', 'X']
+        self.assertTrue(self.game.winner(2, 'X'))
 
     def test_check_second_diagonal_win(self):
-        self.board.rows = [[' ', ' ', 'X'], [' ', 'X', ' '], ['X', ' ', ' ']]
-        self.assertTrue(self.board.winner(6, ' '))
+        self.game.board = [' ', ' ', 'X', ' ', 'X', ' ', 'X', ' ', ' ']
+        self.assertFalse(self.game.winner(5, 'X'))
 
 
 if __name__ == '__main__':
